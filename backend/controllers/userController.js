ad356891390257dbf8935747546aeae94b150d36
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-06-27 19:27:26 +0500

    admin user delete, get, update done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,4 +104,55 @@ const updateCurrentUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { createUser, loginUser, logoutUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile };
+const deleteUserById = asyncHandler(async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (user) {
+        if (user.isAdmin) {
+            res.status(400);
+            throw new Error("Cannot delete an admin user");
+        }
+
+        await User.deleteOne({ _id: user._id })
+
+        res.json({ message: "User deleted successfully" });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select("-password");
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+
+const updateUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+
+export { createUser, loginUser, logoutUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserById, getUserById, updateUserById };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { createUser, loginUser, logoutUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile } from "../controllers/userController.js";
-import {authenticate, authorizeAdmin} from "../middlewares/authMiddleware.js"
+import { createUser, loginUser, logoutUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserById, getUserById, updateUserById } from "../controllers/userController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js"
 
 const router = express.Router();
 
@@ -10,5 +10,8 @@ router.post("/auth", loginUser);
 router.post("/logout", logoutUser);
 
 router.route("/profile").get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+
+// admin routes
+router.route("/:id").delete(authenticate, authorizeAdmin, deleteUserById).get(authenticate, authorizeAdmin, getUserById).put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
